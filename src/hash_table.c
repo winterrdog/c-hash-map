666aad6a61c44ae646475bf4e44fa48d436aebbe
winterrdog <winterrdog@protonmail.ch>
2024-04-21 15:22:55 +0300

    feat: added hashtable insertion

--- src/hash_table.c
+++ src/hash_table.c
@@ -50,6 +50,39 @@ void ht_delete_hash_table(ht_hash_table* tab)
     return;
 }
 
+static ht_item* ht_find_entry(ht_hash_table* table, int index)
+{
+    ht_item* item = table->items[index];
+    return item ? item : NULL;
+}
+
+void ht_insert(ht_hash_table* table, str key, str value)
+{
+    ht_item* item = ht_create_item(key, value);
+    if (!item) {
+        fprintf(stderr, "failed to insert item due to low memory\n");
+        return;
+    }
+
+    int index = ht_get_hash(key, table->size, 0);
+    ht_item* curr_item = ht_find_entry(table, index);
+    // check if index entry is empty
+    if (!curr_item)
+        goto INSERT_INTO_TABLE;
+
+    // search for an empty entry
+    int i = 1;
+    while (curr_item) {
+        index = ht_get_hash(curr_item->key, table->size, i);
+        curr_item = ht_find_entry(table, index);
+        ++i;
+    }
+
+INSERT_INTO_TABLE:
+    table->items[index] = item;
+    table->count++;
+}
+
 void free_space(void* ptr)
 {
     if (!ptr) {
